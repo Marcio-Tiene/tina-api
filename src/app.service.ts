3d9be6ea8b2c79320b308b642a33bf3d25ad4ab1
Marcio Tiene <marcio.ricardo@mettzer.com>
2021-06-13 23:26:07 -0300

    Familiarizing with nestjs

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(name = 'World'): { greetings: string } {
+    const capitalizedName = name.charAt(0).toLocaleUpperCase() + name.slice(1);
+
+    return { greetings: `Hello ${capitalizedName}!` };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function serverInit() {
+  try {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.use(helmet());
+    await app.listen(4000);
+    console.log('Server started');
+  } catch (err) {
+    console.error(`Error Starting server due ${err.message}`);
+  }
 }
-bootstrap();
+serverInit();
